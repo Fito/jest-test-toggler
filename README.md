cb29169a7c69d7380dac57e17b22ec27f180eb78
Fito von Zastrow <adolfovonzastrow@Fitos-MacBook-Pro.local>
2017-06-25 16:04:25 -0700

    Initial commit. Basic toggling implemented.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Jest Test Toggler package
+
+Toggle between your Jest test files and subject files.
+This add-on supports `.js` and `.jsx` files
---
+++ spec/jest-test-toggler-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import JestSpecOpenerView from '../lib/jest-spec-opener-view';
+
+describe('JestSpecOpenerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
